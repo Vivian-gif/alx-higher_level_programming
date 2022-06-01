596df1cf63a7b6935eaf32e50f8197a83e56ce3f
lucheey V <voluchi06@gmail.com>
2022-06-01 08:03:52 +0100

    Complete this source code to print object-oriented programming with Python, followed by a new line

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an inter language that combines remarkable power with verypreted, interactive, object-oriented programming\
+ clear syntax"
+str = str[39:66] + str[106:112] + str[:6]
+print(str)
