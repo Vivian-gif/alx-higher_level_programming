69f60ad23d819ab4e7231d108297c2ccfcca7429
lucheey V <voluchi06@gmail.com>
2022-06-01 07:18:46 +0100

    A Shell script that runs a Python script

---
+++ 0x00-python-hello_world/lists.h
@@ -0,0 +1,18 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <suct listint_s *next;
+} listint_t;
+
+size_t print_litdlib.h>
+
+
+typedef struct listint_s
+{
+	int n;
+	strstint(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+void free_listint(listint_t *head);
+int check_cycle(listint_t *list);
+
+#endif /* LISTS_H */
